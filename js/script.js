23bad8319f7221e7689d88b7030d01d73be86f82
Bianca Veleanu <63341026+BiancaVVV@users.noreply.github.com>
2023-04-18 15:36:40 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,16 +4,19 @@ let profile = document.querySelector('.header .flex .profile');
 document.querySelector('#menu-btn').onclick = () =>{
    navbar.classList.toggle('active');
    profile.classList.remove('active');
+   
 }
 
 document.querySelector('#user-btn').onclick = () =>{
    profile.classList.toggle('active');
    navbar.classList.remove('active');
+   
 }
 
 window.onscroll = () =>{
    navbar.classList.remove('active');
    profile.classList.remove('active');
+   
 }
 
 let mainImage = document.querySelector('.quick-view .box .row .image-container .main-image img');
@@ -23,5 +26,8 @@ subImages.forEach(images =>{
    images.onclick = () =>{
       src = images.getAttribute('src');
       mainImage.src = src;
+      
    }
+   
 });
+
